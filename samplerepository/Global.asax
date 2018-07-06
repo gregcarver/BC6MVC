94497123df276936488685188bff45319a360490
gregory.carverprovalus <gregory.carverprovalus@gmail.com>
2018-07-06 13:22:04 -0500

    Add project files.

---
+++ samplerepository/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="samplerepository.MvcApplication" Language="C#" %>
---
+++ samplerepository/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(samplerepository.Startup))]
+namespace samplerepository
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
